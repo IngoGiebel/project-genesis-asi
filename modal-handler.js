f60e48ab7cc5046e52d8d018a2c8d56ae3433ee7
Ingo Giebel <ingo.giebel@gmail.com>
2025-06-13 13:25:38 +0200

    modal-handler.js reformatted

--- modal-handler.js
+++ modal-handler.js
@@ -1,50 +1,49 @@
 function setupCitationModal() {
-  const citeButton = document.querySelector("a[href$='#citationModal']");
-  const modalElement = document.getElementById("citationModal");
+  const citeButton = document.querySelector("a[href$='#citationModal']")
+  const modalElement = document.getElementById("citationModal")
 
   if (citeButton && modalElement) {
-    const citationModal = new bootstrap.Modal(modalElement);
+    const citationModal = new bootstrap.Modal(modalElement)
 
     citeButton.addEventListener(
       "click",
       event => {
-        event.preventDefault();
-        citationModal.show();
-      });
+        event.preventDefault()
+        citationModal.show()
+      })
 
-    const copyButton = modalElement.querySelector(".code-copy-button");
-    const codeBlock = modalElement.querySelector("pre.sourceCode code");
+    const copyButton = modalElement.querySelector(".code-copy-button")
+    const codeBlock = modalElement.querySelector("pre.sourceCode code")
 
     if (copyButton && codeBlock) {
       copyButton.addEventListener(
         "click",
         () => {
-          const textToCopy = codeBlock.innerText;
+          const textToCopy = codeBlock.innerText
           navigator.clipboard.writeText(textToCopy).then(() => {
-            const originalIcon = copyButton.innerHTML;
-            copyButton.innerHTML = "<i class='bi bi-check2'></i>";
-            copyButton.title = "Copied!";
+            const originalIcon = copyButton.innerHTML
+            copyButton.innerHTML = "<i class='bi bi-check2'></i>"
+            copyButton.title = "Copied!"
             // Revert after 2 seconds
             setTimeout(
               () => {
-                copyButton.innerHTML = originalIcon;
-                copyButton.title = "Copy to clipboard";
+                copyButton.innerHTML = originalIcon
+                copyButton.title = "Copy to clipboard"
               },
-              2000);
+              2000)
           }).catch(err => {
-            console.error("Failed to copy text using navigator.clipboard: ", err);
+            console.error("Failed to copy text using navigator.clipboard: ", err)
             // You could add user feedback here, e.g., changing button text to "Error"
-          });
-        }
-      );
+          })
+        },
+      )
     }
   }
 }
 
 // Ensure DOM is ready before setting up the modal
 if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", setupCitationModal);
-}
-else {
-  setupCitationModal();
+  document.addEventListener("DOMContentLoaded", setupCitationModal)
+} else {
+  setupCitationModal()
 }
